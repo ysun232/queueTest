e9ca080822926299f97e032b44b0d94a5e71f437
SUN YILONG <YSUN232@HOTMAIL.COM>
2018-10-02 15:54:52 -0400

    added more lines, functionalities to the java file

--- queueTest.java
+++ queueTest.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+//for a queue, the first element in is also the first element out
 public class queueTest{
     
     public static void main(String[] args){
@@ -17,10 +18,24 @@ public class queueTest{
         //checking the elements in my queue
         System.out.println("Hello "+ aQueue);
         
+        //try out the poll method, which returns null instead of no such element exception
+        String aQueuePoll = aQueue.poll();
+        System.out.println("I will remove the first element, which is " +aQueuePoll +", and the new queue would be "+ aQueue);
+        //removes the first element in, and returns it
         
+        //I can also check for if an element is present in the queue
+        String isIn = "Yilong Sun";
         
+        if (aQueue.contains(isIn)){
+        	System.out.println("The queue contains "+isIn);
+        } else{
+        	System.out.println("The queue does not contain" + isIn);
+        }
         
-        
+        //using element() or peek() will look at the first element in the queue and return it witohut removing it
+        //both these two lines should return the same answer
+        System.out.println("The first element is "+aQueue.peek());
+        System.out.println("The first element is "+aQueue.element());
         
     }
 }
